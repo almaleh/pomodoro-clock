6d9ab15f0cb04438252308788b2f17076a3ea167
BesherAlMaleh <besher.m@gmail.com>
2019-05-09 22:57:34 -0400

    implement counters, add sound

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -6,39 +6,91 @@ class Pomodoro extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            breakLength: props.breakLength,
-            sessionLength: props.sessionLength,
-            durationLeft: props.durationLeft
-        };
-        this.adjustBreakTime = this.adjustBreakTime.bind(this); 
-        this.adjustSessionTime = this.adjustSessionTime.bind(this); 
+            sessionTimer: undefined
+        }
+        console.log(JSON.stringify(props));
 
-        console.log(JSON.stringify(props)); 
-    }
-
-    updateStateFrom(props) {
-        this.setState({
-            breakLength: props.breakLength,
-            sessionLength: props.sessionLength,
-            durationLeft: props.durationLeft
-        });
-
-        console.log(JSON.stringify(props)); 
+        // "The Great Binding"
+        this.adjustBreakTime = this.adjustBreakTime.bind(this);
+        this.adjustSessionTime = this.adjustSessionTime.bind(this);
+        this.startTimer = this.startTimer.bind(this);
+        this.resetTimer = this.resetTimer.bind(this);
+        this.updateSession = this.updateSession.bind(this);
     }
 
     componentWillReceiveProps(props) {
-        this.updateStateFrom(props); 
+        this.updateStateFrom(props);
+    }
+
+    updateStateFrom(props) {
+        // console.log(JSON.stringify(props)); 
     }
 
     adjustBreakTime(increase) {
-        increase ? this.props.increaseBreak() : this.props.decreaseBreak(); 
+        if (this.props.sessionStarted) { return; }
+        increase ? this.props.increaseBreak() : this.props.decreaseBreak();
     }
 
     adjustSessionTime(increase) {
-        increase ? this.props.increaseSession() : this.props.decreaseSession(); 
+        if (this.props.sessionStarted) { return; }
+        increase ? this.props.increaseSession() : this.props.decreaseSession();
     }
 
+    startTimer() {
+        this.stopTimer();
+        if (this.props.sessionStarted == false) {
+            this.props.start();
+            this.setState({
+                sessionTimer: window.setInterval(this.updateSession, 1000)
+            })
+        }
+    }
 
+    stopTimer() {
+        this.props.pause();
+        window.clearTimeout(this.state.sessionTimer)
+    }
+
+    resetTimer() {
+        this.audioBeep.pause();
+        this.audioBeep.currentTime = 0;
+        this.stopTimer();
+        this.props.reset();
+    }
+
+    getSessionLabel() {
+        return this.props.breakInProgress ? "Break" : "Session";
+    }
+
+    getSessionTime() {
+        let value = this.props.breakInProgress ? this.props.breakDurationLeft : this.props.durationLeft;
+        let float = parseFloat(value).toFixed(2);
+        let string = String(float).replace(".", ":");
+        if (string.length < 5) {
+            string = "0" + string;
+        }
+        return string;
+    }
+
+    updateSession() {
+        let isBreak = this.props.breakInProgress;
+        let timeLeft = isBreak ? this.props.breakDurationLeft : this.props.durationLeft;
+
+        if (timeLeft <= 0) {
+            this.props.toggleBreak();
+            this.audioBeep.play(); 
+            return;
+        }
+
+
+        let newValue = Number((timeLeft - 0.01).toFixed(2));
+        // clip seconds to 60 max
+        if (parseInt(newValue) !== parseInt(newValue + 0.4)) {
+            newValue -= 0.4;
+        }
+
+        isBreak ? this.props.updateBreak(newValue) : this.props.updateSession(newValue);
+    }
 
     render() {
         return (
@@ -47,30 +99,32 @@ class Pomodoro extends React.Component {
                 <div id="settings">
 
                     <div id="break-settings" className="settings-half">
-                        <h3>Break Length</h3>
-                        <button onClick={() => this.adjustBreakTime(true)} className="button">⬆</button>
-                        <p id="break-time">{this.state.breakLength}</p>
-                        <button onClick={() => this.adjustBreakTime(false)}className="button">⬇</button>
+                        <h3 id="break-label">Break Length</h3>
+                        <button id="break-increment" onClick={() => this.adjustBreakTime(true)} className="button">⬆</button>
+                        <p id="break-length">{this.props.breakLength}</p>
+                        <button id="break-decrement" onClick={() => this.adjustBreakTime(false)} className="button">⬇</button>
                     </div>
 
                     <div id="session-settings" className="settings-half">
-                        <h3>Session Length</h3>
-                        <button onClick={() => this.adjustSessionTime(true)} className="button">⬆</button>
-                        <p id="session-time">{this.state.sessionLength}</p>
-                        <button onClick={() => this.adjustSessionTime(false)} className="button">⬇</button>
+                        <h3 id="session-label">Session Length</h3>
+                        <button id="session-increment" onClick={() => this.adjustSessionTime(true)} className="button">⬆</button>
+                        <p id="session-length">{this.props.sessionLength}</p>
+                        <button id="session-decrement" onClick={() => this.adjustSessionTime(false)} className="button">⬇</button>
                     </div>
 
                 </div>
 
                 <div id="session-display">
-                    <h2>Session</h2>
-                    <h2>25:00</h2>
+                    <h2 id="timer-label">{this.getSessionLabel()}</h2>
+                    <h2 id="time-left">{this.getSessionTime()}</h2>
                 </div>
 
                 <div id="controls">
-                    <button className="button"><i className="fas fa-play"></i></button>
-                    <button className="button"><i className="fas fa-power-off"></i></button>
+                    <button className="button" id="start_stop" onClick={this.startTimer}><i className="fas fa-play"></i></button>
+                    <button className="button" id="reset" onClick={this.resetTimer}><i className="fas fa-power-off"></i></button>
                 </div>
+
+                <audio id="beep" src="https://goo.gl/65cBl1" ref={(audio) => { this.audioBeep = audio; }} />
 
             </header>
         )
--- src/Redux.js
+++ src/Redux.js
@@ -4,42 +4,92 @@ const INC_BREAK = 'INC_BREAK';
 const DEC_BREAK = 'DEC_BREAK';
 const INC_SESS = 'INC_SESS';
 const DEC_SESS = 'DEC_SESS';
+const START = 'START';
+const PAUSE = 'PAUSE';
 const RESET = 'RESET';
+const UPDATE = 'UPDATE';
+const TOGGLE_BREAK = 'TOGGLE_BREAK';
+const UPDATE_BREAK = 'UPDATE_BREAK';
 
 const defaultState = {
     breakLength: 5,
+    breakDurationLeft: 5,
     sessionLength: 25,
-    durationLeft: 25
+    durationLeft: 25,
+    sessionStarted: false,
+    breakInProgress: false
 }
 
 const reducer = (state = defaultState, action) => {
     switch (action.type) {
         case INC_BREAK:
             {
-                let newState = { breakLength: state.breakLength + 1 };
+                let newBreak = Math.min(state.breakLength + 1, 60);
+                let newState = {
+                    breakLength: newBreak,
+                    breakDurationLeft: newBreak
+                };
                 return Object.assign({}, state, newState);
             }
         case DEC_BREAK:
             {
-                let newBreak = Math.max(state.breakLength - 1, 0); 
-                let newState = { breakLength: newBreak };
+                let newBreak = Math.max(state.breakLength - 1, 1);
+                let newState = {
+                    breakLength: newBreak,
+                    breakDurationLeft: newBreak
+                };
                 return Object.assign({}, state, newState);
             }
         case INC_SESS:
             {
-                let newState = { sessionLength: state.sessionLength + 1 };
+                let newSession = Math.min(state.sessionLength + 1, 60);
+                let newState = {
+                    sessionLength: newSession,
+                    durationLeft: newSession
+                };
                 return Object.assign({}, state, newState);
             }
         case DEC_SESS:
             {
-                let newSession = Math.max(state.sessionLength - 1, 0); 
-                let newState = { sessionLength: newSession };
+                let newSession = Math.max(state.sessionLength - 1, 1);
+                let newState = {
+                    sessionLength: newSession,
+                    durationLeft: newSession
+                };
+                return Object.assign({}, state, newState);
+            }
+        case START:
+            {
+                let newState = { sessionStarted: true };
+                return Object.assign({}, state, newState);
+            }
+        case PAUSE:
+            {
+                let newState = { sessionStarted: false };
                 return Object.assign({}, state, newState);
             }
         case RESET:
             {
-                let newState = { durationLeft: 25 };
-                return Object.assign({}, state, newState);
+                return defaultState;
+            }
+        case UPDATE:
+            {
+                let newState = { durationLeft: action.durationLeft }
+                return Object.assign({}, state, newState);;
+            }
+        case TOGGLE_BREAK:
+            {
+                let newState = { 
+                    durationLeft: state.sessionLength,
+                    breakDurationLeft: state.breakLength,
+                    breakInProgress: !state.breakInProgress 
+                }
+                return Object.assign({}, state, newState);;
+            }
+        case UPDATE_BREAK:
+            {
+                let newState = { breakDurationLeft: action.breakDurationLeft }
+                return Object.assign({}, state, newState);;
             }
         default: return state;
     }
@@ -69,9 +119,41 @@ const decreaseSession = () => {
     }
 }
 
+const start = () => {
+    return {
+        type: START
+    }
+}
+
+const pause = () => {
+    return {
+        type: PAUSE
+    }
+}
+
 const reset = () => {
     return {
         type: RESET
+    }
+}
+
+const updateSession = (durationLeft) => {
+    return {
+        type: UPDATE,
+        durationLeft
+    }
+}
+
+const toggleBreak = () => {
+    return {
+        type: TOGGLE_BREAK,
+    }
+}
+
+const updateBreak = (breakDurationLeft) => {
+    return {
+        type: UPDATE_BREAK,
+        breakDurationLeft
     }
 }
 
@@ -81,7 +163,10 @@ const mapStateToProps = (state) => {
     return {
         breakLength: state.breakLength,
         sessionLength: state.sessionLength,
-        durationLeft: state.durationLeft
+        durationLeft: state.durationLeft,
+        breakDurationLeft: state.breakDurationLeft,
+        sessionStarted: state.sessionStarted,
+        breakInProgress: state.breakInProgress
     }
 }
 
@@ -99,9 +184,24 @@ const mapDispatchToProps = (dispatch) => {
         decreaseSession: () => {
             dispatch(decreaseSession());
         },
+        start: () => {
+            dispatch(start());
+        },
+        pause: () => {
+            dispatch(pause());
+        },
         reset: () => {
             dispatch(reset());
         },
+        updateSession: (durationLeft) => {
+            dispatch(updateSession(durationLeft));
+        },
+        toggleBreak: () => {
+            dispatch(toggleBreak());
+        },
+        updateBreak: (breakDurationLeft) => {
+            dispatch(updateBreak(breakDurationLeft));
+        }
     }
 }
 
